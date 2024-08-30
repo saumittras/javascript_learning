9e35f8fb6ac42af7d161f8cb8f39af7828208efc
saumittras <saumittra.eee@gmail.com>
2024-08-30 22:28:10 +0600

    added task 5

---
+++ nan.js
@@ -0,0 +1,7 @@
+// isNaN() function
+//  not number , and can not be transformed into number = true
+// number or can be transformed into number = false
+// var x = "saumittra";
+var x = 6;
+
+console.log(isNaN(x));
---
+++ task_5_day_2.js
@@ -0,0 +1,16 @@
+/***
+
+you have two numbers in two variables, called: num1, num2
+
+now declare a variable called result. 
+if num1 is bigger than num2 then result will be double of num1. if not, then the value of the variable result will be the sum of num1 and num2.
+
+write a simple if-else. 
+
+also, write it using ternary operator.
+
+ */
+let num1 = 50;
+let num2 = 40;
+
+num1 > num2 ? console.log(num1 * 2) : console.log(num1 + num2);
